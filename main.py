0acb896ee837512ec8e3ead80dba5dbb2890ab3e
Jeremy Zhou <jzhou@connect.kellettschool.com>
2025-04-01 02:51:04 +0000

    Apr 1, 2025, 2:51 AM

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # testing
+def is_odd(num):
+    counter = 0
+    even = True
+    while counter < num:
+        counter += 1
+        if even:
+            even = False
+        else:
+            even = True
+    return not even
+
