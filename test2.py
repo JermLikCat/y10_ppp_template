b9221ac8aae680657430216db051ddd9e435c192
Jeremy Zhou <jzhou@connect.kellettschool.com>
2025-06-16 02:07:59 +0000

    Jun 16, 2025, 2:07 AM

---
+++ test2.py
@@ -0,0 +1,14 @@
+def labinit():
+    print("woof woof")
+def shibainit():
+    print("woof woof")
+def printlabspecies():
+    print("Lab")
+def printshibaspecies():
+    print("Shiba")
+
+def main():
+    labinit()
+    printlabspecies()
+    shibainit()
+    printshibaspecies()
